23d9bc8a716b527872b460fc82bb04cc068d3348
Unknown <kamilbaginski7@gmail.com>
2018-06-13 17:27:32 +0200

    Auto stash before merge of "master" and "origin/master"

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import { StyleSheet, Text, View, Animated } from 'react-native';
-
+import { createBottomTabNavigator } from 'react-navigation';
 import { API_KEY } from './WeatherAPIKey';
 import Weather from './Weather';
 
-export default class App extends React.PureComponent {
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!</Text>
+        </View>
+    );
+  }
+}
+
+class SettingsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Settings!</Text>
+      </View>
+    );
+  }
+}
+
+
+
+class App extends React.Component {
   state = {
     isLoading: true,
     temperature: 0,
@@ -44,12 +66,19 @@ export default class App extends React.PureComponent {
     const { isLoading } = this.state;
     return (
       <View style={styles.container}>
-        {isLoading ? <Text>Fetching The Weather</Text> : 
+        {isLoading ? <Text>Fetching The Weather</Text> :
         <Weather weather={this.state.weatherCondition} temperature={this.state.temperature}/>}
       </View>
     );
   }
 }
+
+export default createBottomTabNavigator({
+  Home: App,
+  Settings: SettingsScreen,
+});
+
+
 
 const styles = StyleSheet.create({
   container: {
