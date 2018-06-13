3bacbfb7086a923ef0515020feaa3c28483bb896
pawelsilverboxit <C:\Users\AdminSBIT\AppData\Roaming\The Bat!>
2018-06-13 16:41:41 +0200

    weather api

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Animated } from 'react-native';
 
-export default class App extends React.Component {
+import { API_KEY } from './WeatherAPIKey';
+import Weather from './Weather';
+
+export default class App extends React.PureComponent {
+  state = {
+    isLoading: true,
+    temperature: 0,
+    weatherCondition: null,
+    error: null
+  };
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.fetchWeather(position.coords.latitude, position.coords.longitude);
+      },
+      error => {
+        this.setState({
+          error: 'Error getting weather condtions'
+        });
+      }
+    );
+  }
+
+  fetchWeather(lat = 25, lon = 25) {
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`
+    ).then  (res => res.json())
+    .then(json => {
+      console.log(json);
+      this.setState({
+        temperature: json.main.temp,
+        weatherCondition: json.weather[0].main,
+        isLoading: false
+      })
+    });
+  }
+
+
   render() {
+    const { isLoading } = this.state;
     return (
       <View style={styles.container}>
-        <Text>Welcome in ogrodnik!</Text>
+        {isLoading ? <Text>Fetching The Weather</Text> : 
+        <Weather weather={this.state.weatherCondition} temperature={this.state.temperature}/>}
       </View>
     );
   }
@@ -15,7 +55,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
